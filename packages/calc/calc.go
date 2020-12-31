a0405e4011032b3a1af6b9560e53ab4cfdb01321
kotaoue <kotaoue@gmail.com>
2020-12-31 14:42:36 +0900

    Update calc.go

--- packages/calc/calc.go
+++ packages/calc/calc.go
@@ -1,6 +1,18 @@
 package calc
 
-// Calc is calcurate two numbers.
-func Calc(a, b int) int {
+type calculator struct{}
+
+// Calculator is the interface that wraps the basic calculate method.
+type Calculator interface {
+	Calc(a, b int) int
+}
+
+// Calc is calculated two numbers.
+func (c *calculator) Calc(a, b int) int {
 	return a + b
+}
+
+// NewCalculator returns a Calculator that calculate inputed numbers.
+func NewCalculator() Calculator {
+	return &calculator{}
 }
