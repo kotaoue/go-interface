5248093a39726216d13a481ce545ff2f5152841c
kotaoue <kotaoue@gmail.com>
2021-01-02 07:19:31 +0900

    Update calc.go

--- packages/calc/calc.go
+++ packages/calc/calc.go
@@ -1,18 +1,21 @@
 package calc
 
-type calculator struct{}
+// Calculator is the struct for execute basic calculate.
+type Calculator struct {
+	alu ALU
+}
 
-// Calculator is the interface that wraps the basic calculate method.
-type Calculator interface {
+// ALU is the interface that wraps the basic calculate method.
+type ALU interface {
 	Calc(a, b int) int
 }
 
 // Calc is calculated two numbers.
-func (c *calculator) Calc(a, b int) int {
+func (c *Calculator) Calc(a, b int) int {
 	return a + b
 }
 
 // NewCalculator returns a Calculator that calculate inputed numbers.
-func NewCalculator() Calculator {
-	return &calculator{}
+func NewCalculator() *Calculator {
+	return &Calculator{}
 }
